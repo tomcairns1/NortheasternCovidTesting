9d64a446eb63568e32ca548daf5b247fdf97e5c5
Tom Cairns <t.cairns97@gmail.com>
2021-12-26 00:18:52 -0500

    Removed extra index

--- bin/createDatabase.py
+++ bin/createDatabase.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 File: 
 createDatabase.py
@@ -26,8 +27,10 @@ def main():
     ''' Business Logic '''
     # Create empty database
     db_filename = 'NUCovid.db'
-
     createEmptyDatabase(db_filename)
+
+    # Import the Data
+    importData(db_filename)
 
 
 def createEmptyDatabase(db_filename):
@@ -39,12 +42,28 @@ def createEmptyDatabase(db_filename):
     '''
     try:
         conn = sqlite3.connect(db_filename)
-        print(sqlite3.version)
+        print(f'The sqlite3 version is: {sqlite3.version}')
     except Error as e:
         print(e)
     finally:
         if conn:
             conn.close()
+
+
+def importData(db_filename):
+    '''
+    Function: importData
+    Parameter: db_filename (str)
+    Purpose: The purpose of this function is to import the data from a
+             CSV file into database table
+    '''
+    # Hardcoding filename for now, will add to pipeline eventually
+    csv_filename = '../data/output.csv'
+    nucase_data = pd.read_csv(csv_filename).drop(columns=['Unnamed: 0'])
+
+    # Import data into nucase table
+    nucase_data.to_sql('nucase', con=sqlite3.Connection(db_filename),
+                       if_exists='replace', index=True)
 
 
 if __name__ == '__main__':
